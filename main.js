95f8231c6657cf4db1834a2ebe7a2bfabb2e62d1
Asus <Asus@DESKTOP-PH6TS4O>
2025-09-05 22:36:07 +0300

    сделал на полоину дз

---
+++ main.js
@@ -0,0 +1,28 @@
+//1.
+// let name = prompt("Enter your name");
+// alert("Hello " + name);
+
+//2.
+// const year = 2025;
+// let yearBorn = prompt("Enter your year: ");
+// let resultAge = year - yearBorn;
+// alert("Your age is " + resultAge);
+
+
+//3.
+
+// let side_of_a_square = prompt("Enter side of a square");
+// alert("side of a square is " + (side_of_a_square * 4));
+
+
+//4.
+// let radius = prompt("Enter radius circle: ");
+// let p = 3.15;
+// let resultArea = radius * p;
+// alert("Area circle = " + resultArea);
+
+//5. 
+// let distance = prompt("Enter a distance: ");
+// let time = prompt("Enter a time: ");
+// let speed = distance / time;
+// alert("The speed you need to travel is = " + speed + " km/h");
